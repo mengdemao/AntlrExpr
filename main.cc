4299274cf21eadfc4d1d3a9bb14226977b8ee8bf
Meng Demao <mengdemao19951021@163.com>
2021-12-10 22:01:40 +0800

    feat: 添加格式化工具       添加clang-format格式化工具       添加clang-tidy配置文件

--- main.cc
+++ main.cc
@@ -7,180 +7,135 @@
  *
  *
  */
-#include <iostream>
-#include <string>
-#include "antlr4-runtime.h"
+#include "ExprBaseListener.h"
 #include "ExprLexer.h"
 #include "ExprParser.h"
-#include "ExprBaseListener.h"
-
-#include <config.h>
-#include <syslog.h>
 #include "antlr4-runtime.h"
+#include <iostream>
+#include <string>
+
+#include "ExprBaseListener.h"
+#include "ExprBaseVisitor.h"
 #include "ExprLexer.h"
 #include "ExprParser.h"
 #include "ExprVisitor.h"
-#include "ExprBaseVisitor.h"
-#include "ExprBaseListener.h"
+#include "antlr4-runtime.h"
+#include <config.h>
+#include <syslog.h>
 
 using namespace antlr4;
 using namespace tree;
+using namespace std;
 
-class ExprTreeListener : public ExprBaseListener {
-private:
+class ExprTreeListener : public ExprBaseListener
+{
+  private:
 	stack<double> ExprStack;
 
-public:
-	void enterProg(ExprParser::ProgContext *ctx) override
-	{
-	}
-	void exitProg(ExprParser::ProgContext *ctx) override
-	{
-	}
-	void enterPrintExpr(ExprParser::PrintExprContext *ctx) override
-	{
-	}
-	void exitPrintExpr(ExprParser::PrintExprContext *ctx) override
-	{
-	}
+  public:
+	void enterProg (ExprParser::ProgContext* ctx) override {}
+	void exitProg (ExprParser::ProgContext* ctx) override {}
+	void enterPrintExpr (ExprParser::PrintExprContext* ctx) override {}
+	void exitPrintExpr (ExprParser::PrintExprContext* ctx) override {}
 
-	void enterAssign(ExprParser::AssignContext *ctx) override
-	{
-	}
+	void enterAssign (ExprParser::AssignContext* ctx) override {}
 
-	void exitAssign(ExprParser::AssignContext *ctx) override
-	{
-	}
+	void exitAssign (ExprParser::AssignContext* ctx) override {}
 
-	void enterBlank(ExprParser::BlankContext *ctx) override
-	{
-	}
+	void enterBlank (ExprParser::BlankContext* ctx) override {}
 
-	void exitBlank(ExprParser::BlankContext *ctx) override
-	{
-	}
+	void exitBlank (ExprParser::BlankContext* ctx) override {}
 
-	void enterParens(ExprParser::ParensContext *ctx) override
-	{
-	}
+	void enterParens (ExprParser::ParensContext* ctx) override {}
 
-	void exitParens(ExprParser::ParensContext *ctx) override
-	{
-	}
+	void exitParens (ExprParser::ParensContext* ctx) override {}
 
-	void enterMulDiv(ExprParser::MulDivContext *ctx) override
-	{
-	}
+	void enterMulDiv (ExprParser::MulDivContext* ctx) override {}
 
-	void exitMulDiv(ExprParser::MulDivContext *ctx) override
-	{
-	}
+	void exitMulDiv (ExprParser::MulDivContext* ctx) override {}
 
-	void enterAddSub(ExprParser::AddSubContext *ctx) override
-	{
-	}
+	void enterAddSub (ExprParser::AddSubContext* ctx) override {}
 
-	void exitAddSub(ExprParser::AddSubContext *ctx) override
-	{
-	}
+	void exitAddSub (ExprParser::AddSubContext* ctx) override {}
 
-	void enterId(ExprParser::IdContext *ctx) override
-	{
-	}
+	void enterId (ExprParser::IdContext* ctx) override {}
 
-	void exitId(ExprParser::IdContext *ctx) override
-	{
-	}
+	void exitId (ExprParser::IdContext* ctx) override {}
 
-	void enterInt(ExprParser::IntContext *ctx) override
-	{
-	}
-	void exitInt(ExprParser::IntContext *ctx) override
-	{
-	}
+	void enterInt (ExprParser::IntContext* ctx) override {}
+	void exitInt (ExprParser::IntContext* ctx) override {}
 
-	void enterEveryRule(antlr4::ParserRuleContext *ctx) override
-	{
-	}
-	void exitEveryRule(antlr4::ParserRuleContext *ctx) override
-	{
-	}
+	void enterEveryRule (antlr4::ParserRuleContext* ctx) override {}
+	void exitEveryRule (antlr4::ParserRuleContext* ctx) override {}
 
-	void visitTerminal(antlr4::tree::TerminalNode *node) override;
-	void visitErrorNode(antlr4::tree::ErrorNode *node) override;
+	void visitTerminal (antlr4::tree::TerminalNode* node) override;
+	void visitErrorNode (antlr4::tree::ErrorNode* node) override;
 };
 
-void ExprTreeListener::visitErrorNode(antlr4::tree::ErrorNode *node)
+void ExprTreeListener::visitErrorNode (antlr4::tree::ErrorNode* node)
 {
-	std::string getText = node->getText();
-	if (!getText.empty())
-	{
-		std::cout << __func__ << "\t" << node->getText() << std::endl;
+	std::string getText = node->getText ();
+	if (!getText.empty ()) {
 	}
 }
 
-void ExprTreeListener::visitTerminal(antlr4::tree::TerminalNode *node)
+void ExprTreeListener::visitTerminal (antlr4::tree::TerminalNode* node)
 {
-	std::string getText = node->getText();
-	if (getText != "\n")
-	{
-		std::cout << __func__ << "\t" << node->getText() << std::endl;
+	std::string getText = node->getText ();
+	if (getText != "\n") {
 	}
 }
 
-class ExprTreeVisitor : public ExprVisitor {
-public:
-	antlrcpp::Any visitProg(ExprParser::ProgContext *context)
+class ExprTreeVisitor : public ExprVisitor
+{
+  public:
+	antlrcpp::Any visitProg (ExprParser::ProgContext* context)
 	{
-		return visitChildren(context);
+		return visitChildren (context);
 	}
 
-	antlrcpp::Any visitPrintExpr(ExprParser::PrintExprContext *context)
+	antlrcpp::Any visitPrintExpr (ExprParser::PrintExprContext* context)
 	{
-		return visitChildren(context);
+		return visitChildren (context);
 	}
 
-	antlrcpp::Any visitAssign(ExprParser::AssignContext *context)
+	antlrcpp::Any visitAssign (ExprParser::AssignContext* context)
 	{
-		return visitChildren(context);
+		return visitChildren (context);
 	}
 
-	antlrcpp::Any visitBlank(ExprParser::BlankContext *context)
+	antlrcpp::Any visitBlank (ExprParser::BlankContext* context)
 	{
-		return visitChildren(context);
+		return visitChildren (context);
 	}
 
-	antlrcpp::Any visitParens(ExprParser::ParensContext *context)
+	antlrcpp::Any visitParens (ExprParser::ParensContext* context)
 	{
-		return visitChildren(context);
+		return visitChildren (context);
 	}
 
-	antlrcpp::Any visitMulDiv(ExprParser::MulDivContext *context)
+	antlrcpp::Any visitMulDiv (ExprParser::MulDivContext* context)
 	{
-		std::string text = context->op->getText();
-		std::cout << __func__ << "\t" << text << std::endl;
-		return visitChildren(context);
+		std::string text = context->op->getText ();
+		return visitChildren (context);
 	}
 
-	antlrcpp::Any visitAddSub(ExprParser::AddSubContext *context)
+	antlrcpp::Any visitAddSub (ExprParser::AddSubContext* context)
 	{
-		std::string text = context->op->getText();
-		std::cout << __func__ << "\t" << text << std::endl;
-		return visitChildren(context);
+		std::string text = context->op->getText ();
+		return visitChildren (context);
 	}
 
-	antlrcpp::Any visitId(ExprParser::IdContext *context)
+	antlrcpp::Any visitId (ExprParser::IdContext* context)
 	{
-		std::string text = context->ID()->getText();
-		std::cout << __func__ << "\t" << text << std::endl;
-		return visitChildren(context);
+		std::string text = context->ID ()->getText ();
+		return visitChildren (context);
 	}
 
-	antlrcpp::Any visitInt(ExprParser::IntContext *context)
+	antlrcpp::Any visitInt (ExprParser::IntContext* context)
 	{
-		std::string text = context->INT()->getText();
-		std::cout << __func__ << "\t" << text << std::endl;
-		return visitChildren(context);
+		std::string text = context->INT ()->getText ();
+		return visitChildren (context);
 	}
 };
 
@@ -190,7 +145,7 @@ public:
  * @param  argv 参数列表
  * @return int  返回数据
  */
-int main(int argc, const char *argv[])
+int main (int argc, const char* argv[])
 {
 	if (!argv[1]) {
 		std::cout << "Antlr expr version " << AntlrExpr_VERSION << std::endl;
@@ -198,35 +153,33 @@ int main(int argc, const char *argv[])
 		return -1;
 	}
 
-	std::string InputString(argv[1]);
-	InputString.append("\n");
+	std::string InputString (argv[1]);
+	InputString.append ("\n");
 
 	// 输入字符串
-	ANTLRInputStream input(InputString);
+	ANTLRInputStream input (InputString);
 
 	// 词法解析
-	ExprLexer lexer(&input);
+	ExprLexer lexer (&input);
 
 	// 分割单词
-	CommonTokenStream tokens(&lexer);
+	CommonTokenStream tokens (&lexer);
 
 	// 解析语法单元
-	ExprParser parser(&tokens);
+	ExprParser parser (&tokens);
 
 	// 生成语法树
-	ParseTree *tree = parser.prog();
+	ParseTree* tree = parser.prog ();
 
 	// 1. Listener模式解析语法树
-	std::cout << "Listener mode start" << std::endl;
 	ExprTreeListener listener;
-	ParseTreeWalker walker;
-	walker.walk(&listener, tree);
-	std::cout << "Listener mode end\n" << std::endl;
+	ParseTreeWalker	 walker;
+	walker.walk (&listener, tree);
 
 	// 2. Visitor模式解析语法树
-	std::cout << "Visitor mode start" << std::endl;
 	ExprTreeVisitor visitor;
-	visitor.visit(tree);
-	std::cout << "Visitor mode end\n" << std::endl;
+	visitor.visit (tree);
+
+	cout << "success" << endl;
 	return 0;
 }
