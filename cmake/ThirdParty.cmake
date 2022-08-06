d550b418b0af272ecd2befa21daee129dd070a9b
Meng Demao <mengdemao19951021@163.com>
2022-08-06 18:08:56 +0800

    * bump third-party version

--- cmake/ThirdParty.cmake
+++ cmake/ThirdParty.cmake
@@ -4,7 +4,7 @@ include(ExternalProject)
 FetchContent_Declare(
 	googletest
 	GIT_REPOSITORY https://github.com/google/googletest.git
-	GIT_TAG        release-1.11.0
+	GIT_TAG        release-1.12.1
 )
 set(BUILD_GMOCK OFF CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(googletest)
@@ -12,6 +12,6 @@ FetchContent_MakeAvailable(googletest)
 FetchContent_Declare(
 	fmt
 	GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-	GIT_TAG        8.1.1
+	GIT_TAG        9.0.0
 )
 FetchContent_MakeAvailable(fmt)
